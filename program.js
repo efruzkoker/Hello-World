d8120c8248261847611f716f3269e2543a839a6a
efruz <efruz@efruz-MacBook-Pro.local>
2017-08-09 13:38:09 +0300

    added Hello World program

---
+++ program.js
@@ -0,0 +1,23 @@
+var http = require("http");
+
+http.createServer(function(request , response){
+// send the http heade
+//http status:200 : ok
+// content type: text/plain
+
+if(request.url=="/eng"){
+	response.write("Hello World!");
+	response.end();
+}
+else if(request.url=='/tr'){
+	response.write("Merhaba Dunya!");
+	response.end();
+}
+else if(request.url=="/"){
+	response.write("Lutfen dil seciniz,url nize ekleyiniz,please insert the url for your language");
+	response.end();
+}
+}).listen(8081);
+
+//console will print the message
+console.log("Server running at http://127.0.0.1:8081");
